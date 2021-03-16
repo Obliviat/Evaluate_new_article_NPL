edc17c483061adbfef321088829ed50ced2c10bf
Joanna <joanna.gil09@gmail.com>
2021-03-16 17:27:30 -0400

    first upload

--- src/server/index.js
+++ src/server/index.js
@@ -11,13 +11,6 @@ const axios = require('axios')
 console.log(`Your API key is ${process.env.API_KEY}`);
 const app = express()
 
-// You could call it aylienapi, or anything else
-var aylien = require("aylien_textapi");
-var textapi = new aylien({
-    application_id: process.env.API_ID,
-    application_key: process.env.API_KEY
-});
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -38,15 +31,6 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-// app.get("/algo", function (req, res) {
-//     console.log(req)
-//     res.json({ name: 'joanna', lastname: 'gil', ruta: req.query.url })
-// })
-
-// app.post("/algo", function (req, res) {
-//     console.log(req)
-//     res.json({ url: req.body.url })
-// })
 
 app.post('/test', (req, res) => {
     console.log(req.body.mysite)
@@ -60,23 +44,23 @@ app.post('/test', (req, res) => {
     });
 });
 
-// app.post('/test', (req, res) => {
+app.post('/test', (req, res) => {
 
-//     axios.post("https://api.meaningcloud.com/sentiment-2.1", {
+    axios.post("https://api.meaningcloud.com/sentiment-2.1", {
 
-//     },
-//         {
-//             params: {
-//                 key: "process.env.API_KEY",
-//                 lang: "en",
-//                 txt: req.body.mysite
-//             }
-//         }).then(function (r) {
+    },
+        {
+            params: {
+                key: "process.env.API_KEY",
+                lang: "en",
+                txt: req.body.mysite
+            }
+        }).then(function (r) {
 
-//             res.json(r.data);
-//         }).catch(function (e) { console.log(e) })
+            res.json(r.data);
+        }).catch(function (e) { console.log(e) })
 
-// });
+});
 
 
 
