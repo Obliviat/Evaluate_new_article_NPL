e9dd99010bd3184d3aa755aa4389a1858c75c031
Joanna <joanna.gil09@gmail.com>
2021-03-16 21:33:07 -0400

    update

--- src/client/index.js
+++ src/client/index.js
@@ -2,14 +2,14 @@ import { requestAPI } from './js/evaluator'
 import { handleSubmit } from './js/formHandler'
 import { isValid } from './js/urlValidator'
 
-// import './styles/resets.scss'
-// import './styles/base.scss'
-// import './styles/footer.scss'
-// import './styles/form.scss'
-// import './styles/header.scss'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
 
-// export {
-//   requestAPI,
-//   handleSubmit,
-//   isValid
-// }
+export {
+  requestAPI,
+  handleSubmit,
+  isValid
+}
