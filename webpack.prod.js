2c8b82b3b2d82b011034f864572cb71ec1b702d6
Joanna <joanna.gil09@gmail.com>
2021-03-17 21:12:48 -0400

    progress call

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,16 @@
 const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/client/index.js',
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   module: {
     rules: [
       {
@@ -12,16 +18,18 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader"
       },
+
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
-    })
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" })
   ]
 }
